a157d4f76e24cbcec48fcd74d6bed9212363bce0
Dipanjan <Dipanjan.Kumar1@gmail.com>
2019-03-28 04:34:16 -0700

    Nunit Tests

---
+++ OutReachDataAccessLayer.Tests/TestEvent.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using OutReachDataAccessLayer.Repository;
+
+namespace OutReachDataAccessLayer.Tests
+{
+    [TestFixture]
+    class TestEvent
+    {
+        EventRepository eventRepository = null;
+        public TestEvent()
+        {
+            eventRepository = new EventRepository();
+        }
+        [Test]
+        public void FindEvent()
+        {
+            Assert.AreEqual(eventRepository.FindEvent("EVNT00047261").Location, "Singapore");
+        }
+    }
+}
---
+++ OutReachDataAccessLayer.Tests/TestRole.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OutReachDataAccessLayer.Models;
+using OutReachDataAccessLayer.Repository;
+
+namespace OutReachDataAccessLayer.Tests
+{
+    [TestFixture]
+    public class TestRole
+    {
+        RoleRepository roleRepository = null;
+        public TestRole()
+        {
+            roleRepository = new RoleRepository();
+        }
+        [Test]
+        public void FindRoleName()
+        {
+            Role role = roleRepository.FindRoleName(3);
+            Assert.That(role.RoleName.Equals("POC"));
+        }
+        [Test]
+        public void FindRoleID()
+        {
+            Role role = roleRepository.FindRoleId("PMO");
+            Assert.That(role.RoleID.Equals(2));
+        }
+    }
+}
