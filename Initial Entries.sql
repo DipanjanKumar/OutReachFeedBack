f4c6e903a974f3c30d4db684807fc11956d8eccd
DipanjanKumar <44995759+DipanjanKumar@users.noreply.github.com>
2019-03-28 05:11:27 -0700

    Documents

---
+++ Initial Entries.sql
@@ -0,0 +1,25 @@
+insert into Roles values('Admin')
+insert into Roles values('PMO')
+insert into Roles values('POC')
+
+insert into MainMenus values('Dashboard')
+insert into MainMenus values('Admin')
+
+insert into SubMenus values('User','User','Index',2)
+insert into SubMenus values('Mail Template','MailTemplate','Index',2)
+insert into SubMenus values('Mail Reminder','MailReminder','Index',2)
+insert into SubMenus values('Dashboard','Dashboard','Index',1)
+
+insert into RoleMenus values(1,1)
+insert into RoleMenus values(2,1)
+insert into RoleMenus values(3,1)
+insert into RoleMenus values(4,1)
+insert into RoleMenus values(4,2)
+insert into RoleMenus values(4,3)
+
+insert into FeedbackQuestions values(1,'Rating')
+insert into FeedbackQuestions values(2,'What did you like about this volunteering activity')
+insert into FeedbackQuestions values(3,'What can be improved for this volunteering activity')
+
+  insert into Users values ('711876','Dipanjan',1,null)
+ insert into Users values ('113833','Amit',2,null)
